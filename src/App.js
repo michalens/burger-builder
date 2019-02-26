bf6f0aaa3810191ba4dff457ee0316eb6b04cb33
Michael <michale.ns32@gmail.com>
2019-02-26 17:16:29 +0200

    add Orders container

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout'
+import Orders from './containers/Orders/Orders'
 import {Route, Switch} from 'react-router-dom'
 
 
@@ -12,7 +13,8 @@ class App extends Component {
         <Layout>
         	<Switch>
         		<Route path='/checkout' component={Checkout} />
-        		<Route path='/' exact component={BurgerBuilder} />
+        		<Route path='/orders' exact component={Orders} />
+            <Route path='/' exact component={BurgerBuilder} />
         	</Switch>
         </Layout>
       </div>
---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import Order from '../../components/Order/Order'
+// import './Orders.css'
+
+class Orders extends Component {
+	render() {
+		return (
+			<div>
+				<Order />
+				<Order />
+			</div>
+		)
+	}
+}
+
+export default Orders;
+	
