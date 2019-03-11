c16010cffaad25f502ec69600a5934102366c5be
Michael <michale.ns32@gmail.com>
2019-03-11 18:11:42 +0000

    add loading and redirect from checkout

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -93,8 +93,7 @@ class ContactData extends Component {
 				valid: true
 			},
 		},
-		formIsValid: false,
-		loading: false
+		formIsValid: false
 	}
 
 	orderHandler = (event) => {
@@ -180,7 +179,7 @@ class ContactData extends Component {
 				<Button btnType='Success' disabled={!this.state.formIsValid}>ORDER</Button>
 			</form>
 		);
-		if (this.state.loading === true) {
+		if (this.props.loading === true) {
 			form = <Spinner />
 		}
 		return (
@@ -195,13 +194,14 @@ class ContactData extends Component {
 const mapStateToProps = state => {
 	return {
 		ings: state.ingredients,
-		price: state.totalPrice
+		price: state.totalPrice,
+		loading: state.loading
 	}
 }
 
 const mapDispatchToProps = dispatch => {
 	return {
-		onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData))
+		onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
 	}
 }
 
