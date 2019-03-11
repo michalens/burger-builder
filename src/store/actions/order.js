c33d30a448d596816a923c1c095c773dfd1f67f5
Michael <michale.ns32@gmail.com>
2019-03-11 18:21:45 +0000

    combine reducers

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -27,7 +27,7 @@ export const purchaseBurger = (orderData) => {
         dispatch(purchaseBurgerStart())
         axios.post('/orders.json', orderData)
 			.then(response => {
-				dispatch(purchaseBurgerSuccess(response.data.id, orderData))
+				dispatch(purchaseBurgerSuccess(response.data.name, orderData))
 			})
 			.catch(err => {
 				dispatch(purchaseBurgerFail(err))
