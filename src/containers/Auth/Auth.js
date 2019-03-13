6d3903d5febf33ef2b0ca2beb97a5578a29dd44c
Michael <michale.ns32@gmail.com>
2019-03-13 18:17:25 +0200

    add actions

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux'
 import Input from '../../components/UI/Input/Input'
 import Button from '../../components/UI/Button/Button'
-
+import * as actions from '../../store/actions'
 import './Auth.css'
 
 class Auth extends Component {
@@ -75,6 +75,11 @@ class Auth extends Component {
         });
 	}
 
+	sumbitHandler = (event) => {
+		event.preventDefault();
+		this.props.onAuth(this.state.controls.email.value, this.state.controls.email.password)
+	}
+
 	render () {
 		const formElementsArray = [];
 		for (let key in this.state.controls) {
@@ -97,8 +102,8 @@ class Auth extends Component {
 			/>
 		))
 		return (
-			<div>
-				<form className='Auth'>
+			<div className='Auth'>
+				<form onSubmit={this.sumbitHandler}>
 					{form}
 					<Button btnType='Success'>SUBMIT</Button>
 				</form>
@@ -107,5 +112,11 @@ class Auth extends Component {
 	}
 }
 
-export default Auth;
+const mapDispatchToProps = dispatch => {
+	return {
+		onAuth: (email, password) => dispatch(actions.auth(email, password))
+	}
+}
+
+export default connect(null, mapDispatchToProps)(Auth);
 	
