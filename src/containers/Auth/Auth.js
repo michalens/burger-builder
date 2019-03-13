9546bf22b55cfe0643abf6d21bc0e5b9b07d675c
Michael <michale.ns32@gmail.com>
2019-03-13 18:05:04 +0200

    add auth form

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react';
+
+import Input from '../../components/UI/Input/Input'
+import Button from '../../components/UI/Button/Button'
+
+import './Auth.css'
+
+class Auth extends Component {
+	state = {
+		controls: {
+			email: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'email',
+					placeholder: 'Email Address'
+				},
+				value: '',
+				validation: {
+					required: true,
+					isEmail: true
+				},
+				valid: false,
+				touched: false
+			},
+			password: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'password',
+					placeholder: 'Password'
+				},
+				value: '',
+				validation: {
+					required: true,
+					minLength: 6
+				},
+				valid: false,
+				touched: false
+			},
+		}
+	}
+
+	checkValidity = (value, rules) => {
+		let isValid = true;
+		
+		if (rules.required) {
+			isValid = value.trim() !== '' && isValid;
+		}
+		
+		if (rules.minLength) {
+			isValid = value.length >= rules.minLength && isValid;
+		}
+		
+		if (rules.maxLength) {
+		isValid = value.length <= rules.maxLength && isValid;
+		}
+		
+		return isValid
+	}
+
+	inputChangedHandler = (event, controlName) => {
+		const eventValue = event.target.value;
+	    this.setState(prevState => {
+            const updatedControls = {
+                ...prevState.controls,
+            };
+            const updatedFormElement = {
+                ...updatedControls[controlName],
+            };
+            updatedFormElement.value = eventValue;
+            updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+            updatedFormElement.touched = true;
+	        updatedControls[controlName] = updatedFormElement;
+
+            return { controls: updatedControls };
+        });
+	}
+
+	render () {
+		const formElementsArray = [];
+		for (let key in this.state.controls) {
+			formElementsArray.push({
+				id: key,
+				config: this.state.controls[key]
+			})
+		}
+
+		const form = formElementsArray.map(formElement => (
+			<Input 
+				key={formElement.id}
+				elementType={formElement.config.elementType} 
+				elementConfig={formElement.config.elementConfig} 
+				value={formElement.config.value}
+				invalid={!formElement.config.valid}
+				shouldValidate={formElement.config.validation}
+				touched={formElement.config.touched}
+				changed={(event) => this.inputChangedHandler(event, formElement.id)}
+			/>
+		))
+		return (
+			<div>
+				<form className='Auth'>
+					{form}
+					<Button btnType='Success'>SUBMIT</Button>
+				</form>
+			</div>
+		)
+	}
+}
+
+export default Auth;
+	
