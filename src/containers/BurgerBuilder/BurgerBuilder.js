8a595ae95a0b466f63de558771c0e3e9260e8932
Michael <michale.ns32@gmail.com>
2019-03-15 08:03:13 +0000

    redirect users to the checkout page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -33,6 +33,7 @@ class BurgerBuilder extends Component {
 		if(this.props.isAuth) {
 			this.setState({purchasing: true})
 		} else {
+			this.props.onSetAuthRedirectPath('/checkout')
 			this.props.history.push('/auth')
 		}
 		
@@ -101,7 +102,8 @@ const mapDispatchToProps = dispatch => {
 		onIngredientAdded: ing => dispatch(actions.addIngredient(ing)),
 		onIngredientRemoved: ing => dispatch(actions.removeIngredient(ing)),
 		onInitIngredients: () => dispatch(actions.initIngredients()),
-		onInitPurchase: () => dispatch(actions.purchaseInit())
+		onInitPurchase: () => dispatch(actions.purchaseInit()),
+		onSetAuthRedirectPath: (path) => dispatch(actions.setAuthRedirectPath(path))
 	}
 }
 
