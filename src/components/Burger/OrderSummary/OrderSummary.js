7e3255365ac4b2d15372426cbacc35a9eed179ed
michael <michale.ns32@gmail.com>
2020-04-12 10:42:47 +0300

    turn to functional comp

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,36 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Button from '../../UI/Button/Button'
 
-//can turn back to functional
-class OrderSummary extends Component {
-	// componentWillUpdate() {
-	// 	console.log()
-	// }
+const OrderSummary = (props) => {
 
-	render() {
-		const ingredientSummary = Object.keys(this.props.ingredients)
-			  .map(igKey => (
-			  	<li key={igKey}>
-			  		<span style={{textTransform: 'capitalize'}}>
-			  			{igKey}:
-			  		</span> 
-			  		{this.props.ingredients[igKey]}
-			  	</li>
-			  	))
-		return (
-			<>
-				<h3>Your Order</h3>
-				<p>A delicious burger with the following ingredients:</p>
-				<ul>
-					{ingredientSummary}
-				</ul>
-				<p><strong>Total Price: ${this.props.price.toFixed(2)}</strong></p>
-				<p>Continue to Checkout?</p>
-				<Button clicked={this.props.purchaseCanceled} btnType={'Danger'}>CANCEL</Button>
-				<Button clicked={this.props.purchaseContinued} btnType={'Success'}>CONTINUE</Button>
-			</>
-		) 	
-	}
+	const ingredientSummary = Object.keys(props.ingredients)
+			.map(igKey => (
+			<li key={igKey}>
+				<span style={{textTransform: 'capitalize'}}>
+					{igKey}:
+				</span> 
+				{props.ingredients[igKey]}
+			</li>
+			))
+	return (
+		<>
+			<h3>Your Order</h3>
+			<p>A delicious burger with the following ingredients:</p>
+			<ul>
+				{ingredientSummary}
+			</ul>
+			<p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
+			<p>Continue to Checkout?</p>
+			<Button clicked={props.purchaseCanceled} btnType={'Danger'}>CANCEL</Button>
+			<Button clicked={props.purchaseContinued} btnType={'Success'}>CONTINUE</Button>
+		</>
+	) 	
 }
 
 export default OrderSummary;
