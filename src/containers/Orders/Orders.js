a7494f8fd5938bdf243644b53a3db09d82628b74
michael <michale.ns32@gmail.com>
2020-04-12 12:30:27 +0300

    use hooks in Orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import {connect} from 'react-redux'
 
 import axios from '../../axios-orders'
@@ -8,29 +8,28 @@ import withErrorHandler from '../../hoc/WithErrorHandler/WithErrorHandler'
 import * as actions from '../../store/actions/index'
 
 
-class Orders extends Component {
+const Orders = props => {
 
-	componentWillMount() {
-		this.props.onFetchOrders(this.props.token, this.props.userId)
-	}
+	useEffect(() => {
+		props.onFetchOrders(props.token, props.userId)
+	}, [])
 
-	render() {
-		let orders = <Spinner />;
-		if (!this.props.loading) {
-			orders = this.props.orders.map(order => (
-					<Order 
-						key={order.id}
-						ingredients={order.ingredients}
-						price={+order.price}
-					/>
-				))
-		}
-		return (
-			<div>
-				{orders}
-			</div>
-		)
+
+	let orders = <Spinner />;
+	if (!props.loading) {
+		orders = props.orders.map(order => (
+				<Order 
+					key={order.id}
+					ingredients={order.ingredients}
+					price={+order.price}
+				/>
+			))
 	}
+	return (
+		<div>
+			{orders}
+		</div>
+	)
 }
 
 const mapStateToProps = state => {
