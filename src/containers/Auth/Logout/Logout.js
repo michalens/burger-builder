441d5e253a33bb35ba89d387fbd807dc59568dc6
michael <michale.ns32@gmail.com>
2020-04-12 11:07:44 +0300

    use hooks

--- src/containers/Auth/Logout/Logout.js
+++ src/containers/Auth/Logout/Logout.js
@@ -1,18 +1,17 @@
-import React, {Component} from 'react'
+import React, { useEffect } from 'react'
 import {Redirect} from 'react-router-dom'
 import {connect} from 'react-redux'
 
 import *  as actions from '../../../store/actions'
 
-class Logout extends Component {
+const Logout = props => {
+
+    useEffect(() => {
+        props.onLogout()
+    }, [])
     
-    componentDidMount() {
-        this.props.onLogout()
-    }
     
-    render() {
-        return <Redirect to="/" />
-    }
+    return <Redirect to="/" />
 }
 
 
